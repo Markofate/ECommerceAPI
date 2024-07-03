997d3bc6dfa138100aacfef62d4dab06f6aab6e9
kemalkunt <114948156+Markofate@users.noreply.github.com>
2024-07-03 13:42:05 +0300

    Add project files.

---
+++ ECommerceAPI/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.Controllers
+{
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        [Route("/products/")]
+        public string GetProducts()
+        {
+            return "Products Listed!";
+        }
+        [HttpGet]
+        [Route("/products/{id}")]
+        public string GetProduct(int id)
+        {
+            return $"Product {id} Listed!";
+        }
+    }
+}
