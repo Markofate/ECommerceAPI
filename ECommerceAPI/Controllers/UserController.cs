b2bc0e673ab867f03677426ab0a1fe636521e6dd
kemalkunt <114948156+Markofate@users.noreply.github.com>
2024-08-05 14:33:04 +0300

    Hangfire added

--- ECommerceAPI/Controllers/UserController.cs
+++ ECommerceAPI/Controllers/UserController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Core.DTOs;
 using Entities.Concrete;
 using Azure.Core;
+using Hangfire;
+using Hangfire.Annotations;
 using Microsoft.IdentityModel.Tokens;
 
 namespace ECommerceAPI.Controllers
@@ -69,15 +71,22 @@ namespace ECommerceAPI.Controllers
                 throw e;
             }
         }
+
+        public void SendWelcomeEmail()
+        {
+            //Welcome mail işlemleri eklenebilir
+        }
         [HttpPost]
         [Route("/Register")]
         public IActionResult Register([FromBody] RegisterDTO request)
         {
+            
             try
             {
                 var user = _userService.Register(request.FirstName, request.LastName, request.Password, request.RePassword, request.Email);
                 if (user != null)
                 {
+                    BackgroundJob.Enqueue(() => SendWelcomeEmail());
                     return Ok(user);
                 }
 
